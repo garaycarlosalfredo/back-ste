8d6a8754e97aef883773a28353522f8831c1d0d3
garaycarlosalfredo <garaycarlosalfredo@gmail.com>
2021-08-22 17:37:02 -0300

    init

---
+++ back-app/src/main/java/com/ste/backapp/SteApplication.java
@@ -0,0 +1,13 @@
+package com.ste.backapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteApplication.class, args);
+	}
+
+}
---
+++ back-app/src/test/java/com/ste/backapp/SteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ste.backapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
